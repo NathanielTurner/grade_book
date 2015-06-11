f0298fff68068c310db9c3200b1a645bf929ba9b
Nathaniel <Turner@Nathaniels-MacBook-Air.local>
2015-06-10 23:04:48 -0400

    Modify routes so login, or logout can be used instead of login/login etc.

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,10 @@
 Rails.application.routes.draw do
+  resources :teachers
+  resources :parents
+  resources :students
+  root 'dash_board#show'
+  get '/login' => 'login#login'
+  post '/login' => 'login#login'
+  get '/logout' => 'login#logout'
+
 end
